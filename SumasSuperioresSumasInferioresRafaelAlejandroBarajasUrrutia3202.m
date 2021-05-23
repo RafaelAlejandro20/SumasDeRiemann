2add498299e3a789064dc6dce211c23c9bd8c1e9
RafaelAlejandro20 <82453734+RafaelAlejandro20@users.noreply.github.com>
2021-05-23 02:45:40 -0500

    Add files via upload

---
+++ SumasSuperioresSumasInferioresRafaelAlejandroBarajasUrrutia3202.m
@@ -0,0 +1,18 @@
+%Titulo: Sumas superiores de Riemann.
+%Nombre: Rafael Alejandro Barajas Urrutia.
+%Grupo: 3202.
+%Fecha: 23 de mayo de 2021.
+%Desarrollar las sumas de las siguientes funciones.
+
+function [int]=SumasRiemannSup(fun,a,b,m)
+  func=inline(fun);
+  incx=(b-a)./m;
+  x = a:incx:b;
+  A=0;
+  
+  fplot(fun);
+  for i = 1:m
+    A =A + abs(incx * (func(a +(i * incx))));
+  endfor
+  
+  disp(['Riemann Sum Sup: ', num2str(A)]);
